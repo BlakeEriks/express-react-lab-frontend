dd41bf0870c81328928dba2a5538f84b9cd4e4b1
Blake Eriks <eblake98@vt.edu>
2021-10-30 07:16:07 -1000

    Complete Projects Page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,33 @@
-const Projects = props => {
-    return <h1>Projects</h1>
+import { useEffect, useState } from "react"
+
+const Projects = ({url}) => {
+
+    const [projects, setProjects] = useState(null)
+
+    const getProjectData = async () => {
+        const response = await fetch(url + 'projects')
+        const data = await response.json()
+        setProjects(data)
+    }
+
+    useEffect( () => getProjectData(), [])
+
+    const loaded = () => {
+        projects.map( projects => 
+            <div>
+                <h1>{project.name}</h1>
+                <img src={project.image} />
+                <a href={project.git}>
+                    <button>GitHub</button>
+                </a>
+                <a href={project.live}>
+                    <button>Live Site</button>
+                </a>
+            </div>    
+        )
+    }
+
+    return projects ? loaded() : <h1>Loading...</h1>
 }
 
 export default Projects
