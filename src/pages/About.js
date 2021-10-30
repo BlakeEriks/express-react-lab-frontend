e4a6b6dc25915d8e09085101cdd648435fba6572
Blake Eriks <eblake98@vt.edu>
2021-10-30 07:11:25 -1000

    Create About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,26 @@
-const About = props => {
-    return <h1>About</h1>
+import { useEffect, useState } from "react"
+
+const About = ({url}) => {
+
+    const [about, setAbout] = useState(null)
+
+    const getAboutData = async () => {
+        const response = await fetch(url + 'about')
+        const data = await repsonse.json()
+        setAbout(data)
+    }
+
+    useEffect( () => getAboutData(), [])
+
+    const loaded = () => {
+        <div>
+            <h2>{about.name}</h2>
+            <h2>{about.email}</h2>
+            <h2>{about.bio}</h2>
+        </div>
+    }
+
+    return about ? loaded() : <h1>Loading</h1>
 }
 
 export default About
