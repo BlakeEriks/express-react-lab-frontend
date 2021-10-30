c19e72fc4c4b53d6bb192f29a6eec996813dd1e6
Blake Eriks <eblake98@vt.edu>
2021-10-30 08:40:59 -1000

    Project component modifications

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,11 +4,18 @@ const Project = ({project}) => {
             <h1>{project.name}</h1>
             <img className='project-img' src={project.image} alt={project.image}/>
             <div className="project-links">
-                <a href={project.git}>
-                    <button>GitHub</button>
+                <a href={project.git} className="project-link">
+                    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" className="link-logo"/>
+                    <span className="link-text">
+                        GitHub
+                    </span>
                 </a>
-                <a href={project.live}>
-                    <button>Live Site</button>
+                
+                <a href={project.live} className="project-link">
+                    <img src="https://static.thenounproject.com/png/2715693-200.png" alt="" className="link-logo"/>
+                    <span className="link-text">
+                        Live Site
+                    </span>
                 </a>
             </div>
         </div>      
